98e6559dccc076144572b2b19f993daa2931383e
MiguelChaparro-Arkano <86129777+MiguelChaparro-Arkano@users.noreply.github.com>
2021-10-14 17:58:50 -0500

    Update java.js

--- js/java.js
+++ js/java.js
@@ -4,4 +4,5 @@ libros = [
     { nombre: "Satanas", paginas: 456, autor: "Mario Mendoza" },
     { nombre: "El Coronel No tien quien le escriba", paginas: 887, autor: "Gabriel Garcia Marquez" },
     { nombre: "Ilona llega con la LLuvia", paginas: 234, autor: "Sergio Cabrera" },
+    { nombre: "Los Vengadores", paginas: 990, autor: "Carla Giraldo" }
 ]
