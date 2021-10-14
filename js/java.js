7358e13b5f0d9bc3371aa78d20af21aa7c252314
Miguel Chaparro <miguel.chaparro@arkanosoft.com>
2021-10-14 17:35:23 -0500

    v1.02 agregado parrafo html, color css y libro en JS

--- js/java.js
+++ js/java.js
@@ -2,4 +2,5 @@ libros = [
     { nombre: "Harry Potter", paginas: 456, autor: "J.K Rowlin" },
     { nombre: "Caballo de Troya", paginas: 776, autor: "JJ Benitez" },
     { nombre: "Satanas", paginas: 456, autor: "Mario Mendoza" },
+    { nombre: "El Coronel No tien quien le escriba", paginas: 887, autor: "Gabriel Garcia Marquez" },
 ]
