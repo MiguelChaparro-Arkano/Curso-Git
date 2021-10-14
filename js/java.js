11ac37425882284b49669308bcc2dd61ed216149
Miguel Chaparro <miguel.chaparro@arkanosoft.com>
2021-10-14 17:39:22 -0500

    Ingreso nuevo libro

--- js/java.js
+++ js/java.js
@@ -3,4 +3,5 @@ libros = [
     { nombre: "Caballo de Troya", paginas: 776, autor: "JJ Benitez" },
     { nombre: "Satanas", paginas: 456, autor: "Mario Mendoza" },
     { nombre: "El Coronel No tien quien le escriba", paginas: 887, autor: "Gabriel Garcia Marquez" },
+    { nombre: "Ilona llega con la LLuvia", paginas: 234, autor: "Sergio Cabrera" },
 ]
