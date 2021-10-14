79dec9738bfc44e4e4e86b881e35204c7175d545
Miguel Chaparro <miguel.chaparro@arkanosoft.com>
2021-10-14 17:17:51 -0500

    V1.0 creacion libros

--- js/java.js
+++ js/java.js
@@ -1,5 +1,5 @@
 libros = [
-    { nombre: "Harry Potter" },
-    { paginas: 456 },
-    { autor: "J.K Rowlin" }
+    { nombre: "Harry Potter", paginas: 456, autor: "J.K Rowlin" },
+    { nombre: "Caballo de Troya", paginas: 776, autor: "JJ Benitez" },
+    { nombre: "Satanas", paginas: 456, autor: "Mario Mendoza" },
 ]
